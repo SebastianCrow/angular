94bc7582d034e2d9adc79288cc96f8ba7bb104e2
Sebastian Crow <sebastian@sebastiancrow.com>
2018-03-06 23:03:04 +0100

    Displaying modal with the full name

--- src/app/my-buttons/my-buttons.component.ts
+++ src/app/my-buttons/my-buttons.component.ts
@@ -1,17 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import {InputOutputService} from "../input-output-service/input-output.service";
+import {NgbModal, ModalDismissReasons} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'my-buttons',
   templateUrl: './my-buttons.component.html',
-  styleUrls: ['./my-buttons.component.scss'],
-  providers: [ InputOutputService ]
+  styleUrls: ['./my-buttons.component.scss']
 })
 export class MyButtonsComponent implements OnInit {
 
-  constructor(private InputOutputService: InputOutputService) { }
+	public closeResult: string;
 
-  ngOnInit() {
-  }
+	constructor(private InputOutputService: InputOutputService, private modalService: NgbModal) { }
 
+	ngOnInit() {
+	}
+
+	public open(content) {
+		this.modalService.open(content).result.then((result) => {
+			this.closeResult = `Closed with: ${result}`;
+		}, (reason) => {
+			this.closeResult = `Dismissed ${MyButtonsComponent.getDismissReason(reason)}`;
+		});
+	}
+
+	private static getDismissReason(reason: any): string {
+		if (reason === ModalDismissReasons.ESC) {
+			return 'by pressing ESC';
+		} else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+			return 'by clicking on a backdrop';
+		} else {
+			return `with: ${reason}`;
+		}
+	}
 }
