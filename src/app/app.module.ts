ccce357faf24b9f4ae25207f7da5c06491ffcaa5
Sebastian Slepowronski <sebastian.slepowronski@navvis.com>
2018-03-06 14:38:44 +0100

    Added base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InputComponent } from './input/input.component';
+import { OutputComponent } from './output/output.component';
+import { ButtonsComponent } from './buttons/buttons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    OutputComponent,
+    ButtonsComponent
   ],
   imports: [
     BrowserModule
