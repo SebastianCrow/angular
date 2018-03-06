91d85725e9bc80065408ea9eb7e66536d2e8cf44
Sebastian Slepowronski <sebastian.slepowronski@navvis.com>
2018-03-06 14:42:01 +0100

    Revert "Added base components"
    This reverts commit ccce357faf24b9f4ae25207f7da5c06491ffcaa5.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { InputComponent } from './input/input.component';
-import { OutputComponent } from './output/output.component';
-import { ButtonsComponent } from './buttons/buttons.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    InputComponent,
-    OutputComponent,
-    ButtonsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
